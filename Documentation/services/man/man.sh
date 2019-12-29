6012858c6ba45e762a7869ecc1fa8b836d2912d4
DavideAG <davideantonino94@gmail.com>
2019-12-29 18:20:40 +0100

    man script done

--- Documentation/services/man/man.sh
+++ Documentation/services/man/man.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 shopt -s -o nounset
 
-count=1
-
+count=0
 filenames=$(ls | grep -I "pcn-*")
+W=()
 
 for filename in $filenames
 do
-	echo $filename
+	W+=($count "$filename")	
+	let "count++"
 done
+
+OPTION=$(whiptail --title "Welcome to Polycube documentation" --menu "Choose a service" 16 78 10 "${W[@]}" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+	count=0
+	for dir in ${filenames[@]}
+	do
+		if [ $count = $OPTION ]; then
+			whiptail --textbox --scrolltext ${dir}/${dir:4}.rst 38 100
+			break
+		else
+			let "count++"
+		fi
+	done
+fi
